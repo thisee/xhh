f466ffb695a3ca2e8e5cd5dc94ca91e35a6b31d4
额. <1213763661@qq.com>
2026-01-14 18:47:09 +0800

    扫码登录未保存xiaoyao-cvs文件

--- apps/user.js
+++ apps/user.js
@@ -257,7 +257,7 @@ export class user extends plugin {
                     let yaml_url = `./plugins/xhh/data/Stoken/${e.user_id}.yaml`;
                     await this.process_files(yaml_url, data_);
                     const yaml_path = './plugins/xiaoyao-cvs-plugin/data/yaml/';
-                    if (!fs.existsSync(yaml_path)) { //fs.mkdirSync(yaml_path, { recursive: true });
+                    if (fs.existsSync(yaml_path)) { //fs.mkdirSync(yaml_path, { recursive: true });
                         yaml_url = `${yaml_path}${e.user_id}.yaml`;
                         for (let k in data_) {
                             data_[k]['userId'] = Number(e.user_id);
